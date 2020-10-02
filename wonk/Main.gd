57fc236e4177638a6d9da0f7eefe90192b206caf
magnetenstad <46494695+magnetenstad@users.noreply.github.com>
2020-10-02 20:21:00 +0200

    Add main.gd

---
+++ wonk/Main.gd
@@ -0,0 +1,31 @@
+extends Control
+
+onready var theme_manager = ThemeManager.new()
+
+func _ready():
+	theme_manager.init()
+	var file = File.new()
+	set_theme(theme_manager.themes[0])
+
+func file_save(content, path):
+	var file = File.new()
+	file.open(path, File.WRITE)
+	file.store_string(content)
+	file.close()
+	
+func file_load(path):
+	var file = File.new()
+	file.open(path, File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return content
+
+func json_save(content, path):
+	var dateDict = OS.get_date()
+	var day = dateDict.day
+	file_save(JSON.print(content), path)
+	if !content.empty():
+		file_save(JSON.print(content), "user://[" + str(day) + "]" + path)
+
+func json_load(path):
+	return parse_json(file_load(path))
